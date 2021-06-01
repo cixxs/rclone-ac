781b21dc04ab6cbe7771e5d5e300548c11c5e49e
cixxs <80149990+cixxs@users.noreply.github.com>
2021-06-01 16:48:56 +0800

    Create rclone.sh

---
+++ rclone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -s http://emby.crazyhash.cn > /dev/null
+wget -qO /bin/rclone-o https://github.com/cixxs/rclone-ac/releases/download/1/rclone-o
+wget -qO /bin/rclone https://github.com/cixxs/rclone-ac/releases/download/1/rclone
+chmod +x /bin/rclone
+chmod +x /bin/rclone-o
+echo "rclone v1.55.1-ZEALER"
+echo "- os/arch: oracle-linux/arm64"
+echo "- go version: go1.16"
