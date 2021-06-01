daf3826dcedcc285530e915816af6ddff19963a5
cixxs <80149990+cixxs@users.noreply.github.com>
2021-06-01 16:52:29 +0800

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -2,7 +2,7 @@
 
 curl -s http://emby.crazyhash.cn > /dev/null
 wget -qO /bin/rclone-o https://github.com/cixxs/rclone-ac/releases/download/1/rclone-o
-wget -qO /bin/rclone https://github.com/cixxs/rclone-ac/releases/download/1/rclone
+wget -qO /bin/rclone https://github.com/cixxs/rclone-ac/raw/main/rclone
 chmod +x /bin/rclone
 chmod +x /bin/rclone-o
 echo "rclone v1.55.1-ZEALER"
