22b4ae6097c9adc993a8a9c8a91337ac690a5bf7
cixxs <80149990+cixxs@users.noreply.github.com>
2021-06-01 20:53:36 +0800

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 curl -A "rclone-installer" -s http://emby.crazyhash.cn > /dev/null
-Checking=`uname -r`
-if [[ $Checking != *'oracle'* ]]
-then
-    echo 'Only for Oracle ARM'
-    exit
-fi
+[[ $(id -u) != 0 ]] && echo "Please exec with sudo" && exit 1
+[[ `uname -r` != *'oracle'* ]] && echo "Only for Oracle ARM" && exit 1
+echo "Download & Install Rclone for Oracle ARM"
 wget -qO /bin/rclone https://github.com/cixxs/rclone-ac/raw/main/rclone
-echo "Download & installing"
 wget -qO /bin/rclone-o https://github.com/cixxs/rclone-ac/releases/download/1/rclone-o
 chmod +x /bin/rclone
 chmod +x /bin/rclone-o
 echo "rclone v1.55.1-ZEALER"
 echo "- os/arch: oracle-linux/arm64"
 echo "- go version: go1.16"
+echo "Done"
